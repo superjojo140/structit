9d5b427c2cb9aab7c67a0e9400325a546902c8d8
superjojo140 <code@superjojo.de>
2017-05-23 13:03:01 +0200

    Change Charsets to UTF-8

--- structit.sql
+++ structit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 21. Mai 2017 um 10:53
+-- Erstellungszeit: 23. Mai 2017 um 13:01
 -- Server-Version: 10.1.22-MariaDB
 -- PHP-Version: 7.1.4
 
@@ -31,7 +31,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `relation_user_structs` (
   `userId` int(11) NOT NULL,
   `structId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Daten für Tabelle `relation_user_structs`
+--
+
+INSERT INTO `relation_user_structs` (`userId`, `structId`) VALUES
+(1, 1);
 
 -- --------------------------------------------------------
 
@@ -45,6 +52,13 @@ CREATE TABLE `structs` (
   `text` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Daten für Tabelle `structs`
+--
+
+INSERT INTO `structs` (`id`, `name`, `text`) VALUES
+(1, 'test1', '{\"nameFromJson\":\"test1\"}');
+
 -- --------------------------------------------------------
 
 --
@@ -53,15 +67,20 @@ CREATE TABLE `structs` (
 
 CREATE TABLE `user` (
   `id` int(100) NOT NULL,
-  `nickname` varchar(30) NOT NULL,
-  `passwort` varchar(100) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `mail` varchar(30) NOT NULL,
+  `nickname` varchar(30) CHARACTER SET latin1 NOT NULL,
+  `passwort` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `name` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `mail` varchar(30) CHARACTER SET latin1 NOT NULL,
   `aktiv` tinyint(1) DEFAULT '0',
   `admin` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Daten für Tabelle `user`
+--
 
+INSERT INTO `user` (`id`, `nickname`, `passwort`, `name`, `mail`, `aktiv`, `admin`) VALUES
+(1, 'superjojo140', '202cb962ac59075b964b07152d234b70', 'Johannes', 'dev@superjojo.de', 1, 1);
 
 --
 -- Indizes der exportierten Tabellen
@@ -93,7 +112,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `structs`
 --
 ALTER TABLE `structs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
